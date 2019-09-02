048bdd0e84a63dfebe6b00c036ab517864117254
Shivam <jalotrashivam9@gmail.com>
2019-09-02 22:18:07 +0530

    Inside get_set_pixel function defined rules to get one channel at a time from RGB channels

--- tryit.py
+++ tryit.py
@@ -1,15 +1,21 @@
 from uwimg import *
 import os
+import shutil
 
 def tryit_made_figures():
     os.mkdir(os.path.join(os.getcwd(), "Tryit_Photos"))
+
 
 # 1. Getting and setting pixels
 def get_set_pixels():
     im = load_image("data/dog.jpg")
     for row in range(im.h):
         for col in range(im.w):
-            set_pixel(im, col, row, 0, 0)
+            # set_pixel takes in these params (*image, column, row, channel, value_to_set)
+            # set_pixel(im, col, row, 0, 0)     # No red      
+            # set_pixel(im, col, row, 1, 0)      # No green
+            set_pixel(im, col, row, 2, 0)      # No blue
+            # print(im[row][col][0])
     save_image(im, "Tryit_Photos/dog_no_red")
 
 # 3. Grayscale image
@@ -42,15 +48,18 @@ def colorspace_and_saturation():
     save_image(im, "Tryit_Photos/dog_saturated")
 
 if __name__ == "__main__":
-    if not os.path.isdir(os.path.join(os.getcwd(), "Tryit_Photos")):
+    if os.path.isdir(os.path.join(os.getcwd(), "Tryit_Photos")):
+        shutil.rmtree(os.path.join(os.getcwd(), "Tryit_Photos"))
+        tryit_made_figures()
+    else:
         tryit_made_figures()
     
     #Utlity Functions been called
     get_set_pixels()
-    grayscaleImage()
-    shiftImage()
-    clampImage()
-    colorspace_and_saturation()
+    # grayscaleImage()
+    # shiftImage()
+    # clampImage()
+    # colorspace_and_saturation()
 
 
 
